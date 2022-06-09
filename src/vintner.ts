cd4bdf5b0952470fff88aa05b00d498ee858f1ae
Quinn <quinnlee971020@gmail.com>
2022-06-09 00:32:05 -0400

    graph Vintner nft

--- src/vintner.ts
+++ src/vintner.ts
@@ -1,108 +1,146 @@
-import { BigInt } from "@graphprotocol/graph-ts"
 import {
-  Vintner,
-  Approval,
-  ApprovalForAll,
-  OwnershipTransferred,
-  Paused,
-  Transfer,
-  Unpaused,
-  onVintnerCreated,
-  onVintnerRevealed
-} from "../generated/Vintner/Vintner"
-import { ExampleEntity } from "../generated/schema"
+  //   TokenIPFSPathUpdated as TokenIPFSPathUpdatedEvent,
+  Transfer as TransferEvent,
+  Vintner as TokenContract,
+} from "../generated/Vintner/Vintner";
 
-export function handleApproval(event: Approval): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
+import { Token, User } from "../generated/schema";
 
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (!entity) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
+export function handleTransfer(event: TransferEvent): void {
+  let token = Token.load(event.params.tokenId.toString());
+  if (!token) {
+    token = new Token(event.params.tokenId.toString());
+    token.creator = event.params.to.toHexString();
+    token.tokenID = event.params.tokenId;
 
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
+    let tokenContract = TokenContract.bind(event.address);
+    token.contentURI = tokenContract.tokenURI(event.params.tokenId);
+    // token.tokenIPFSPath = tokenContract.getTokenIPFSPath(event.params.tokenId);
+    token.name = tokenContract.name();
+    token.createdAtTimestamp = event.block.timestamp;
   }
+  token.owner = event.params.to.toHexString();
+  token.save();
 
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
-
-  // Entity fields can be set based on event parameters
-  entity.owner = event.params.owner
-  entity.approved = event.params.approved
-
-  // Entities can be written to the store with `.save()`
-  entity.save()
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.BASE_URI(...)
-  // - contract.MASTER_VINTNER_TYPE(...)
-  // - contract.MASTER_VINTNER_YIELD(...)
-  // - contract.NUM_GEN0_VINTNERS(...)
-  // - contract.NUM_GEN1_VINTNERS(...)
-  // - contract.PROMOTIONAL_VINTNERS(...)
-  // - contract.VINTNERS_PER_VINTAGEWINE_MINT_LEVEL(...)
-  // - contract.VINTNER_PRICE_AVAX(...)
-  // - contract.VINTNER_TYPE(...)
-  // - contract.VINTNER_YIELD(...)
-  // - contract.balanceOf(...)
-  // - contract.batchedVintnersOfOwner(...)
-  // - contract.currentVINTAGEWINEMintCost(...)
-  // - contract.getApproved(...)
-  // - contract.getType(...)
-  // - contract.getYield(...)
-  // - contract.isApprovedForAll(...)
-  // - contract.mintingStartedAVAX(...)
-  // - contract.mintingStartedVINTAGEWINE(...)
-  // - contract.name(...)
-  // - contract.owner(...)
-  // - contract.ownerOf(...)
-  // - contract.paused(...)
-  // - contract.startTimeAVAX(...)
-  // - contract.startTimeVINTAGEWINE(...)
-  // - contract.supportsInterface(...)
-  // - contract.symbol(...)
-  // - contract.tokenByIndex(...)
-  // - contract.tokenOfOwnerByIndex(...)
-  // - contract.tokenTypes(...)
-  // - contract.tokenURI(...)
-  // - contract.totalSupply(...)
-  // - contract.typeYields(...)
-  // - contract.vintageWine(...)
-  // - contract.vintnerTypeOracleAddress(...)
-  // - contract.vintnersMinted(...)
-  // - contract.vintnersMintedPromotional(...)
-  // - contract.vintnersMintedWithAVAX(...)
-  // - contract.vintnersMintedWithVINTAGEWINE(...)
-  // - contract.wineryAddress(...)
+  let user = User.load(event.params.to.toHexString());
+  if (!user) {
+    user = new User(event.params.to.toHexString());
+    user.save();
+  }
 }
 
-export function handleApprovalForAll(event: ApprovalForAll): void {}
+//   export function handleTokenURIUpdated(event: TokenIPFSPathUpdatedEvent): void {
+//     let token = Token.load(event.params.tokenId.toString());
+//     if (!token) return
+//     token.tokenIPFSPath = event.params.tokenIPFSPath;
+//     token.save();
+//   }
 
-export function handleOwnershipTransferred(event: OwnershipTransferred): void {}
+// import { BigInt } from "@graphprotocol/graph-ts"
+// import {
+//   Vintner,
+//   Approval,
+//   ApprovalForAll,
+//   OwnershipTransferred,
+//   Paused,
+//   Transfer,
+//   Unpaused,
+//   onVintnerCreated,
+//   onVintnerRevealed
+// } from "../generated/Vintner/Vintner"
+// import { ExampleEntity } from "../generated/schema"
 
-export function handlePaused(event: Paused): void {}
+// export function handleApproval(event: Approval): void {
+//   // Entities can be loaded from the store using a string ID; this ID
+//   // needs to be unique across all entities of the same type
+//   let entity = ExampleEntity.load(event.transaction.from.toHex())
 
-export function handleTransfer(event: Transfer): void {}
+//   // Entities only exist after they have been saved to the store;
+//   // `null` checks allow to create entities on demand
+//   if (!entity) {
+//     entity = new ExampleEntity(event.transaction.from.toHex())
 
-export function handleUnpaused(event: Unpaused): void {}
+//     // Entity fields can be set using simple assignments
+//     entity.count = BigInt.fromI32(0)
+//   }
 
-export function handleonVintnerCreated(event: onVintnerCreated): void {}
+//   // BigInt and BigDecimal math are supported
+//   entity.count = entity.count + BigInt.fromI32(1)
 
-export function handleonVintnerRevealed(event: onVintnerRevealed): void {}
+//   // Entity fields can be set based on event parameters
+//   entity.owner = event.params.owner
+//   entity.approved = event.params.approved
+
+//   // Entities can be written to the store with `.save()`
+//   entity.save()
+
+//   // Note: If a handler doesn't require existing field values, it is faster
+//   // _not_ to load the entity from the store. Instead, create it fresh with
+//   // `new Entity(...)`, set the fields that should be updated and save the
+//   // entity back to the store. Fields that were not set or unset remain
+//   // unchanged, allowing for partial updates to be applied.
+
+//   // It is also possible to access smart contracts from mappings. For
+//   // example, the contract that has emitted the event can be connected to
+//   // with:
+//   //
+//   // let contract = Contract.bind(event.address)
+//   //
+//   // The following functions can then be called on this contract to access
+//   // state variables and other data:
+//   //
+//   // - contract.BASE_URI(...)
+//   // - contract.MASTER_VINTNER_TYPE(...)
+//   // - contract.MASTER_VINTNER_YIELD(...)
+//   // - contract.NUM_GEN0_VINTNERS(...)
+//   // - contract.NUM_GEN1_VINTNERS(...)
+//   // - contract.PROMOTIONAL_VINTNERS(...)
+//   // - contract.VINTNERS_PER_VINTAGEWINE_MINT_LEVEL(...)
+//   // - contract.VINTNER_PRICE_AVAX(...)
+//   // - contract.VINTNER_TYPE(...)
+//   // - contract.VINTNER_YIELD(...)
+//   // - contract.balanceOf(...)
+//   // - contract.batchedVintnersOfOwner(...)
+//   // - contract.currentVINTAGEWINEMintCost(...)
+//   // - contract.getApproved(...)
+//   // - contract.getType(...)
+//   // - contract.getYield(...)
+//   // - contract.isApprovedForAll(...)
+//   // - contract.mintingStartedAVAX(...)
+//   // - contract.mintingStartedVINTAGEWINE(...)
+//   // - contract.name(...)
+//   // - contract.owner(...)
+//   // - contract.ownerOf(...)
+//   // - contract.paused(...)
+//   // - contract.startTimeAVAX(...)
+//   // - contract.startTimeVINTAGEWINE(...)
+//   // - contract.supportsInterface(...)
+//   // - contract.symbol(...)
+//   // - contract.tokenByIndex(...)
+//   // - contract.tokenOfOwnerByIndex(...)
+//   // - contract.tokenTypes(...)
+//   // - contract.tokenURI(...)
+//   // - contract.totalSupply(...)
+//   // - contract.typeYields(...)
+//   // - contract.vintageWine(...)
+//   // - contract.vintnerTypeOracleAddress(...)
+//   // - contract.vintnersMinted(...)
+//   // - contract.vintnersMintedPromotional(...)
+//   // - contract.vintnersMintedWithAVAX(...)
+//   // - contract.vintnersMintedWithVINTAGEWINE(...)
+//   // - contract.wineryAddress(...)
+// }
+
+// export function handleApprovalForAll(event: ApprovalForAll): void {}
+
+// export function handleOwnershipTransferred(event: OwnershipTransferred): void {}
+
+// export function handlePaused(event: Paused): void {}
+
+// export function handleTransfer(event: Transfer): void {}
+
+// export function handleUnpaused(event: Unpaused): void {}
+
+// export function handleonVintnerCreated(event: onVintnerCreated): void {}
+
+// export function handleonVintnerRevealed(event: onVintnerRevealed): void {}
